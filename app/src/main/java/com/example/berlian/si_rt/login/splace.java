0dd0a73edc5fc5b6219025ef79e1bd9579973d5a
berlian <berliangigihp@gmail.com>
2017-07-09 21:43:33 +0700

    progress bar

--- app/src/main/java/com/example/berlian/si_rt/login/splace.java
+++ app/src/main/java/com/example/berlian/si_rt/login/splace.java
@@ -1,4 +1,4 @@
-package com.example.berlian.si_rt;
+package com.example.berlian.si_rt.login;
 
 import android.content.Intent;
 import android.os.Handler;
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
+import com.example.berlian.si_rt.R;
 import com.example.berlian.si_rt.login.SessionManager;
 import com.example.berlian.si_rt.login.login;
 
